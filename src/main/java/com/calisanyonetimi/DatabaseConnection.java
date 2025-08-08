b6403de12e85997cc0aaf2e82bd660552dfc2180
SadikBoraErteni <ertenibora@gmail.com>
2025-08-08 11:26:06 +0300

    .Update: Şifreyi ortam değişkeniyle okumak için kodu güncelledim.

--- src/main/java/com/calisanyonetimi/DatabaseConnection.java
+++ src/main/java/com/calisanyonetimi/DatabaseConnection.java
@@ -8,8 +8,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "mysecretpassword";
-
+private static final String PASSWORD = System.getenv("DB_PASSWORD");
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
